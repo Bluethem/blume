ca51b6db56a8647b3766df8c845eaad6cd56dd95
Bluethem <143272687+Bluethem@users.noreply.github.com>
2025-10-25 19:13:04 -0500

    Implementacion reset - icons

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -20,6 +20,10 @@ export const routes: Routes = [
     path: 'forgot-password',
     loadComponent: () => import('./pages/forgot-password/forgot-password.component').then(m => m.ForgotPasswordComponent)
   },
+  {
+    path: 'reset-password',
+    loadComponent: () => import('./pages/reset-password/reset-password.component').then(m => m.ResetPasswordComponent)
+  },
   // Dashboards protegidos
   {
     path: 'dashboard',
