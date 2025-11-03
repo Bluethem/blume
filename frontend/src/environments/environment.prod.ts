d1764545c02a872a7887e614205dd78cc2054197
Bluethem <143272687+Bluethem@users.noreply.github.com>
2025-11-03 16:40:45 -0500

    Configuracoin environment

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://blume-0pim.onrender.com/api/v1', // ✅ URL de tu backend en Render
+  appName: 'Blume',
+  tokenKey: 'token',
+  userKey: 'user'
+};
